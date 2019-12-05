23961984fe0abec947fe3e682896f55c6607b542
Daryl Duckmanton <Daryl.Duckmanton@tabcorp.com.au>
2019-12-05 14:52:54 +1100

    Updated code for creating an initial stack for S3

--- lib/website-stack.ts
+++ lib/website-stack.ts
@@ -1,26 +1,23 @@
 import { Construct, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
 import { Bucket } from '@aws-cdk/aws-s3';
-import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
 
 export class WebsiteStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     
-    const siteDomain = `${this.node.tryGetContext('subdomain')}.${this.node.tryGetContext('domain')}`;
+    const domain = "lyraddigital.com";
+    const subdomain = "test";
+
+    const siteDomain = `${subdomain}.${domain}`;
 
     // S3 Bucket for hosting
-    const websiteBucket = new Bucket(this, 'WebsiteBucket', {
+    new Bucket(this, 'WebsiteBucket', {
       bucketName: siteDomain,
       websiteIndexDocument: 'index.html',
       publicReadAccess: true,
 
       // Keep for all environments except production
       removalPolicy: RemovalPolicy.DESTROY
-    });
-
-    new BucketDeployment(this, 'DeployFilesToBucket', {
-      sources: [Source.asset('src')],
-      destinationBucket: websiteBucket
     });
   }
 }
