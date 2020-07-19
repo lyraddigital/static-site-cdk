db7678b06cc92c66937b2ec56315925b8317a963
daryl-roadhouse <daryl@roadhouse.com.au>
2020-07-19 17:44:32 +1000

    Updated libary and have it running via environment

--- bin/app.ts
+++ bin/app.ts
@@ -4,6 +4,11 @@ import { WebsiteStack } from '../lib/website/website-stack';
 import { PipelineStack } from '../lib/pipeline/pipeline-stack';
 
 const app = new cdk.App();
-new WebsiteStack(app, 'WebsiteStack');
+new WebsiteStack(app, 'WebsiteStack', { 
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
+});
 
 //new PipelineStack(app, 'PipelineStack');
